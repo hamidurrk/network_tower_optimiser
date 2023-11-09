bd1e300f0404f7726ff6618b4184cb467f415d14
hamidurrk <hamidurrk@gmail.com>
2023-11-09 18:20:09 +0600

    issues fixed. png created

--- generate_data.py
+++ generate_data.py
@@ -2,10 +2,10 @@ import csv
 import random
 import math
 
-x_start, x_end = 23.749249, 23.724104   # longitude
-y_start, y_end = 90.369372, 90.404603   # latitude
+x_start, x_end = 23.7492490, 23.7241040   # longitude
+y_start, y_end = 90.3693720, 90.4046030   # latitude
 signal_start, signal_end = 0, 30
-size = 50
+size = 5000
 dim = round(math.sqrt(size))
 
 x_step = (x_end - x_start) / (dim)
@@ -14,13 +14,15 @@ y_step = (y_end - y_start) / (dim)
 data = []
 
 for i in range(dim):
-    x = round(x_start + i * x_step, 6)
+    x = round(x_start + i * x_step, 7)
     for j in range(dim):
-        y = round(y_start + j * y_step, 6)
+        y = round(y_start + j * y_step, 7)
         signal_strength = random.randint(signal_start, signal_end)
         data.append([x, y, signal_strength])
 
-with open('data.csv', 'w', newline='') as file:
+with open('data2.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Longitude", "Latitude", "Signal_Strength"])
     writer.writerows(data)
+
+# from scipy stats.norm
--- visualize_mpl.py
+++ visualize_mpl.py
@@ -8,13 +8,13 @@ x = []
 y = []
 signal_strength = []
 
-with open('data.csv', 'r') as file:
+with open('final_data.csv', 'r') as file:
     reader = csv.reader(file)
     next(reader)  
     for row in reader:
         x.append(float(row[0]))
         y.append(float(row[1]))
-        signal_strength.append(int(row[2]))
+        signal_strength.append(float(row[2]))
         
 x = np.array(x)
 y = np.array(y)
@@ -28,12 +28,13 @@ grid_x, grid_y = np.mgrid[min(x):max(x):100j, min(y):max(y):100j]
 
 grid_z = griddata((x, y), signal_strength, (grid_x, grid_y), method='cubic')
 
-plt.figure(figsize=(10, 8))
-plt.imshow(grid_z.T, extent=(min(x), max(x), min(y), max(y)), origin='lower')
-plt.title('Interpolated Signal Strength')
-plt.colorbar(label='Signal Strength')
+# plt.figure(figsize=(10, 8))
+# plt.imshow(grid_z.T, extent=(min(x), max(x), min(y), max(y)), origin='lower')
+# plt.title('Interpolated Signal Strength')
+# plt.colorbar(label='Signal Strength')
 
 plt.figure(figsize=(10, 8))
 ax = plt.axes(projection='3d')
 ax.plot_surface(grid_x, grid_y, grid_z, cmap='plasma')
 plt.show()
+# plt.savefig('my_plot.png', dpi=300, bbox_inches='tight')
